dd76017f9bced94f6eefc101e0d30e647fb9363a
jihoon-gh <jihoon806@gmail.com>
2022-08-03 17:46:47 +0900

    0.0.6 version

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,6 +1,7 @@
-# health_check.sh
+# switch.sh
 
 #!/bin/bash
+
 # Crawl current connected port of WAS
 CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
@@ -16,19 +17,12 @@ else
 fi
 
 
-echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
+# Change proxying port into target port
+ echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
 
-for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
-do
-    echo "> #${RETRY_COUNT} trying..."
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/health)
+ echo "> Now Nginx proxies to ${TARGET_PORT}."
 
-    if [ ${RESPONSE_CODE} -eq 200 ]; then
-        echo "> New WAS successfully running"
-        exit 0
-    elif [ ${RETRY_COUNT} -eq 10 ]; then
-        echo "> Health check failed."
-        exit 1
-    fi
-    sleep 10
-done
+ # Reload nginx
+ sudo service nginx reload
+
+ echo "> Nginx reloaded."
