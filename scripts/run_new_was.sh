1482eb240380028cc3c08d0b465570f7b78705de
jihoon-gh <jihoon806@gmail.com>
2022-08-27 00:14:01 +0900

    update shell script

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,6 +22,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-sudo nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/weato-logging/build/libs/weato-0.0.1-SNAPSHOT.jar > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/weato-logging/build/libs/weato-0.0.1-SNAPSHOT.jar > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
