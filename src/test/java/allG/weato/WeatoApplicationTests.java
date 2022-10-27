4f8e30831c223483cc48e4e91c6ff161ec0a3ea8
jihoon-gh <jihoon806@gmail.com>
2022-10-27 23:58:48 +0900

    FEAT CommentTest

--- src/test/java/allG/weato/WeatoApplicationTests.java
+++ src/test/java/allG/weato/WeatoApplicationTests.java
@@ -3,11 +3,12 @@ package allG.weato;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Scanner;
+
 @SpringBootTest
 class WeatoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
